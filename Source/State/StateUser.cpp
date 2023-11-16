3f5421f16d81f02ae322f9db6b9bcf3342ca8274
Alex Kovács <kovacsalex95@gmail.com>
2023-11-16 20:17:44 +0100

    Removed debug line

--- Source/State/StateUser.cpp
+++ Source/State/StateUser.cpp
@@ -2,7 +2,6 @@
 
 StateUser::StateUser(StateManager* stateManager)
 {
-    std::cout << "Signal receiver initiated" << std::endl;
     this->stateManager = stateManager;
     this->stateManager->getSignalManager()->registerReceiver(this);
 }
