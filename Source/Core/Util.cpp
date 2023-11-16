d4e047e3d2f5dcb0feff07421d53eb673bd26be8
Alex Kovács <kovacsalex95@gmail.com>
2023-11-16 10:00:21 +0100

    Added Util class

---
+++ Source/Core/Util.cpp
@@ -0,0 +1,2 @@
+#import "Util.h"
+
---
+++ Source/Core/Util.h
@@ -0,0 +1,16 @@
+#ifndef ZIPLODE_UTIL_H
+#define ZIPLODE_UTIL_H
+
+#import <string>
+#include <wx/wx.h>
+#include <wx/filename.h>
+#include <fmt/format.h>
+
+using std::string;
+
+class Util
+{
+};
+
+
+#endif //ZIPLODE_UTIL_H
