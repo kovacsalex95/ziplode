f31a8ba7b1b5ddf01ccc964b51ae83ffda5ce198
Alex Kovács <kovacsalex95@gmail.com>
2023-11-16 19:45:35 +0100

    Added string pad and datetime formatting

--- Source/Util/Util.cpp
+++ Source/Util/Util.cpp
@@ -1,4 +1,4 @@
-#import "Util.h"
+#include "Util.h"
 
 string Util::normalizePath(const string& path) {
     wxFileName* fileName = new wxFileName(wxString(path));
@@ -29,4 +29,58 @@ string Util::getFormattedFilesize(long long filesize, int decimals)
     currentSize = round(currentSize * divisor) / divisor;
 
     return fmt::format("{} {}", currentSize, sizeUnits[unitIndex]);
+}
+
+string Util::stringPad(string str, int width, HorizontalDirection direction, char padChar)
+{
+    if (str.length() >= width) {
+        return str;
+    }
+
+    int characters = width - (int)str.length();
+    string padString;
+
+    for (int i=0; i<characters; i++) {
+        padString += padChar;
+    }
+
+    switch (direction) {
+        case ZL_HDIR_LEFT: return padString + str;
+        case ZL_HDIR_RIGHT: return str + padString;
+    }
+
+    return str;
+}
+
+string Util::stringPad(int str, int width, HorizontalDirection direction, char padChar)
+{
+    return stringPad(to_string(str), width, direction, padChar);
+}
+
+string Util::getFormattedDateTime(tm dateTime, bool date, bool time, bool second)
+{
+    string formattedDate;
+
+    if (date) {
+        formattedDate += fmt::format("{}.{}.{}.",
+                                     stringPad(1900 + dateTime.tm_year, 4, ZL_HDIR_LEFT, '0'),
+                                     stringPad(1 + dateTime.tm_mon, 2, ZL_HDIR_LEFT, '0'),
+                                     stringPad(dateTime.tm_mday, 2, ZL_HDIR_LEFT, '0'));
+    }
+
+    if (date && time) {
+        formattedDate += ' ';
+    }
+
+    if (time) {
+        formattedDate += fmt::format("{}:{}",
+                                     stringPad(dateTime.tm_hour, 2, ZL_HDIR_LEFT, '0'),
+                                     stringPad(dateTime.tm_min, 2, ZL_HDIR_LEFT, '0'));
+
+        if (second) {
+            formattedDate += ':' + stringPad(dateTime.tm_sec, 2, ZL_HDIR_LEFT, '0');
+        }
+    }
+
+    return formattedDate;
 }
--- Source/Util/Util.h
+++ Source/Util/Util.h
@@ -1,18 +1,25 @@
 #ifndef ZIPLODE_UTIL_H
 #define ZIPLODE_UTIL_H
 
-#import <string>
+#include <string>
+#include <ctime>
 #include <wx/wx.h>
 #include <wx/filename.h>
 #include <fmt/format.h>
 
+#include "../Enum/Directions.h"
+
 using std::string;
+using std::to_string;
 
 class Util
 {
     public:
         static string normalizePath(const string& path);
         static string getFormattedFilesize(long long filesize, int decimals = 3);
+        static string getFormattedDateTime(tm dateTime, bool date = true, bool time = true, bool second = false);
+        static string stringPad(string str, int width, HorizontalDirection direction = ZL_HDIR_RIGHT, char padChar = ' ');
+        static string stringPad(int str, int width, HorizontalDirection direction = ZL_HDIR_RIGHT, char padChar = ' ');
 };
 
 
