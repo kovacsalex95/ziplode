712e300af1adc389ddc5e498c0dc6dbf2752199e
Alex Kovács <kovacsalex95@gmail.com>
2023-11-16 19:45:12 +0100

    Added directions enum

---
+++ Source/Enum/Directions.h
@@ -0,0 +1,16 @@
+#ifndef ZIPLODE_DIRECTIONS_H
+#define ZIPLODE_DIRECTIONS_H
+
+enum HorizontalDirection
+{
+    ZL_HDIR_LEFT = 1,
+    ZL_HDIR_RIGHT = 2,
+};
+
+enum VerticalDirection
+{
+    ZL_VDIR_UP = 1,
+    ZL_VDIR_DOWN = 2,
+};
+
+#endif //ZIPLODE_DIRECTIONS_H
